32ddbaa13bbcef5642d4f03d46caad5e94a581e1
rafael <rafaelcnunes@icloud.com>
2023-01-29 03:42:26 -0300

    added config package

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { UsersModule } from './users/users.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://mongo:27017/test'),
     UsersModule,
     HealthModule,
+    ConfigModule.forRoot(),
   ],
   controllers: [AppController],
   providers: [AppService],
